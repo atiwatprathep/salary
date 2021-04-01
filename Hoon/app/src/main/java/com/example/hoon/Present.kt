47bcc50db5b17d7946a21fc6a1f0c1846cc2a5cc
atiwatprathep <77892874+atiwatprathep@users.noreply.github.com>
2021-04-02 01:22:55 +0700

    first commit

---
+++ Hoon/app/src/main/java/com/example/hoon/Present.kt
@@ -0,0 +1,11 @@
+package com.example.hoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Present : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_present)
+    }
+}
